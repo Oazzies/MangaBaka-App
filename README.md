1778bb060f27677439269831240c94ef81b1feb2
Oazzies <oazziesmail@gmail.com>
2026-04-11 13:10:10 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# MangaBaka App
+
+An un-official MangaBaka app for all platforms
---
+++ android/app/src/main/kotlin/com/example/mangabaka_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.mangabaka_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
