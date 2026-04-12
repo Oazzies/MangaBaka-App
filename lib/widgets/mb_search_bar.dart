72ba8c7513bfbc82c3554b2335c9b1c4dec9472c
Oazzies <oazziesmail@gmail.com>
2026-04-12 23:27:38 +0200

    [feat] beta version of the search bar with actual styling

--- lib/widgets/mb_search_bar.dart
+++ lib/widgets/mb_search_bar.dart
@@ -2,18 +2,41 @@ import 'package:flutter/material.dart';
 
 class MBSearchBar extends StatelessWidget {
   final ValueChanged<String> onChanged;
+  final ValueChanged<String>? onSubmitted;
 
-  const MBSearchBar({Key? key, required this.onChanged}) : super(key: key);
+  const MBSearchBar({
+    Key? key,
+    required this.onChanged,
+    this.onSubmitted,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return TextField(
       decoration: InputDecoration(
-        hintText: "Search...",
-        prefixIcon: Icon(Icons.search),
-        border: OutlineInputBorder(),
+        hintText: "Search for something",
+        hintStyle: const TextStyle(color: Colors.white),
+        prefixIcon: const Icon(Icons.search, color: Colors.white),
+        filled: true,
+        fillColor: const Color(0xFF18181B),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(40),
+          borderSide: BorderSide.none,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(40),
+          borderSide: BorderSide.none,
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(40),
+          borderSide: BorderSide.none,
+        ),
+        contentPadding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
       ),
+      style: const TextStyle(color: Colors.white),
       onChanged: onChanged,
+      onSubmitted: onSubmitted,
+      textInputAction: TextInputAction.search,
     );
   }
 }
