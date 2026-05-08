f8e0258c9714abc5eb784a612f7053ae8e938fb5
Oazzies <oazziesmail@gmail.com>
2026-05-08 14:59:59 +0200

    [fix] migration issues

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -312,6 +312,7 @@ class LibraryEntriesDao extends DatabaseAccessor<AppDatabase>
               progressChapter: Value(e.progressChapter),
               progressVolume: Value(e.progressVolume),
               numberOfRereads: Value(e.numberOfRereads),
+              rating: Value(e.rating),
               seriesId: e.series.id,
             ),
           ),
@@ -375,7 +376,30 @@ class AppDatabase extends _$AppDatabase {
   AppDatabase._() : super(_openConnection());
 
   @override
-  int get schemaVersion => 1;
+  int get schemaVersion => 2;
+
+  @override
+  MigrationStrategy get migration {
+    return MigrationStrategy(
+      onUpgrade: (m, from, to) async {
+        if (from < 2) {
+          // Add missing columns to SeriesTable
+          await m.addColumn(seriesTable, seriesTable.mergedWith);
+          await m.addColumn(seriesTable, seriesTable.contentRating);
+          await m.addColumn(seriesTable, seriesTable.type);
+          await m.addColumn(seriesTable, seriesTable.rating);
+          await m.addColumn(seriesTable, seriesTable.finalVolume);
+          await m.addColumn(seriesTable, seriesTable.totalChapters);
+          await m.addColumn(seriesTable, seriesTable.lastUpdated);
+          await m.addColumn(seriesTable, seriesTable.relationships);
+          await m.addColumn(seriesTable, seriesTable.source);
+          
+          // Add missing columns to LibraryEntriesTable
+          await m.addColumn(libraryEntriesTable, libraryEntriesTable.rating);
+        }
+      },
+    );
+  }
 
   static final AppDatabase _instance = AppDatabase._();
 
